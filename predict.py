b6003e205f2123a1831058c1912c3bc8aab6d96e
Vignesh Palaniappan <vignesh.palani96@gmail.com>
2016-11-07 20:15:22 -0800

    Main Function

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 import json
 from datetime import datetime
@@ -5,59 +6,65 @@ from datetime import timedelta
 
 class Nasa:
 
-	def __init__(self):
-		self.lat = raw_input("Which is the latitude of the location?\n")
-		self.long = raw_input("What is the longitude of the location?\n")
-		self.parameters = {"lon": self.long, "lat": self.lat, "api_key":"9Jz6tLIeJ0yY9vjbEUWaH9fsXA930J9hspPchute"}
+	def __init__(self, latitude, longitude):
+		try:
+			self.lat = int(latitude) #raw_input("Which is the latitude of the location?\n")
+			self.long = int(longitude) #raw_input("What is the longitude of the location?\n")
+			if ((-90 <= self.lat <= 90)and (-180 <= self.long <= 180)):
+				self.success = False
+				sys.exit("The location entered is invalid.")
+			self.parameters = {"lon": self.long, "lat": self.lat, "api_key":"9Jz6tLIeJ0yY9vjbEUWaH9fsXA930J9hspPchute"} #parameters 
+			self.success = True
+		except:
+			self.success = False
+			sys.exit("The location entered is invalid.")
  
 	def getRecent(self):
-		response = requests.get("https://api.nasa.gov/planetary/earth/imagery?", params = self.parameters)
-		imageryDate = json.loads(response.content)["date"]
-		#imageryDate = response.content.split()[1] #get date from response
-		#self.recent = imageryDate[imageryDate.find('"')+1:imageryDate.find(',')-1] #between quotes
-		self.recent = datetime.strptime(imageryDate, "%Y-%m-%dT%H:%M:%S")
-		#print self.recent
-		#print "-----------------------BREAK"
+		response = requests.get("https://api.nasa.gov/planetary/earth/imagery?", params = self.parameters) 
+		imageryDate = json.loads(response.content)["date"] #get recent image date
+		self.recent = datetime.strptime(imageryDate, "%Y-%m-%dT%H:%M:%S") #format to datetime
 
 	def getAverage(self):
 		response = requests.get("https://api.nasa.gov/planetary/earth/assets?", params = self.parameters)
-		totalDates = json.loads(response.content)["count"]
-		totalResults = sorted(json.loads(response.content)["results"])
-		#print totalResults[0]["date"]
-		#assetDate = response.content.split("{")[1]
-		#totalDates = assetDate[assetDate.find(":")+2:assetDate.find(",")] 
-		totalDays = 0
-		#print response.content
-		for i in range(0,int(totalDates)-1):
-			count +=1
-			currentDate = totalResults[i]["date"]
-			#assetDate = response.content.split("{")[i] #current date
-			#currentDate = assetDate[assetDate.find(":")+3:assetDate.find(",")-1] 
-			currentDate = datetime.strptime(currentDate, "%Y-%m-%dT%H:%M:%S")
-			#assetDate = response.content.split("{")[i+1] #next date
-			#nextDate = assetDate[assetDate.find(":")+3:assetDate.find(",")-1] 
-			nextDate = totalResults[i+1]["date"]
-			nextDate = datetime.strptime(nextDate, "%Y-%m-%dT%H:%M:%S")
+		totalDates = json.loads(response.content)["count"] #get count of images
+		totalResults = sorted(json.loads(response.content)["results"]) #sorted list by dates of results
+		totalDays = 0 #day counter
+		for i in range(0,int(totalDates)-1): #loop through all
+			currentDate = totalResults[i]["date"] #get current date
+			currentDate = datetime.strptime(currentDate, "%Y-%m-%dT%H:%M:%S") #formate to datetime
+			nextDate = totalResults[i+1]["date"] #get next date
+			nextDate = datetime.strptime(nextDate, "%Y-%m-%dT%H:%M:%S") #format to datetime
 			difference = abs(nextDate-currentDate).days #find difference 
-			totalDays += difference
-		self.average = totalDays/(int(totalDates)-1)
-		print self.average
+			totalDays += difference #add to total day counter
+		self.average = totalDays/(int(totalDates)-1) #find average day count
 
-	def flyby(self):
-		self.nextPhoto = self.recent + timedelta(days=int(self.average))
-		print "The next time %s latitude %s longitude will be shot is %s." % (self.lat, self.long, self.nextPhoto)
+	def calculateNext(self):
+		self.nextPhoto = self.recent + timedelta(days=int(self.average)) #add average date to recent date 
+		print "The next time (%s latitude ,%s longitude) will be shot is %s." % (self.lat, self.long, self.nextPhoto)
 
+	def __del__(self):
+		if self.success == True:
+			print "Successfuly found the next time a satellite image will be taken.\n"
+		else:
+			print "Failed to find the next time a satellite image will be taken.\n"
 
+def flyBy(latitude, longitude):
+	Location = Nasa(latitude,longitude)
+	Location.getRecent()
+	Location.getAverage()
+	Location.calculateNext()
+	del Location
 
-	#def __del__(self):
-	#	print self.lat
-	#	print self.long
+def main():
+	#flyBy("a", 100)
+	flyBy(90, 2)
+	#flyBy(36.098592,-112.097796) #Grand Canyon
+	#flyBy(43.078154,-79.075891) #Niagra Falls
+	#flyBy(36.998979,-109.045183) #Four Corners Monument
+	#flyBy(37.7937007,-122.4039064) #Delphix San Francisco
 
-
-Location = Nasa()
-Location.getRecent()
-Location.getAverage()
-Location.flyby()
+if __name__ == "__main__":
+    main()
 
 #del Location
 
